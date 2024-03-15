642b42091fbcf014aa6647a4d87ed81f3184dac1
mushybones <joakimdanke@gmail.com>
2024-03-15 11:40:56 +0100

    ijwef

---
+++ todo-script.js
@@ -0,0 +1,34 @@
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("new-tasks");
+
+function addTask(userInput){
+  let listItem = document.createElement("li");
+  let checkBox = document.userInput;
+  checkBox.setAttribute("type", "checkbox");
+  listItem.appendChild(checkBox);
+
+  let taskText = document.createElement("span");
+  taskText.textContent = userInput;
+  listItem.appendChild(taskText);
+  
+  todoList.appendChild(listItem);
+}
+
+
+todoForm.addEventListener("submit", 
+  function (event) {
+    event.preventDefault();
+
+    let newTask =  todoInput.value;
+
+    if(newTask === ""){
+      alert("Please enter a non-empty task");
+      return;
+    }
+
+    addTask(newTask);
+    todoInput.value = "";
+  }
+  
+);
